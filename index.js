ab9bc3f88e5c601c91b061c6ed78bb0949106b4f
TheoVitkovskiy <fvitkovski@mail.de>
2017-12-12 11:12:45 +0100

    Make the app deployable

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 })
 // app.use((err, req, res, next) => {
 //     res.status(424).send({ error: err.message });
